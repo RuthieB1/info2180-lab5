b501386f9b7628baffb10c14096370b65b114654
Ruth-Ann Blair <blair.ruthie1@gmail.com>
2022-11-14 22:55:18 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -3,6 +3,23 @@ window.onload = function(){
     countrylookup.addEventListener("click", function(){
         let country = document.getElementById("country").value;
         let url = "http://localhost/info2180-lab5/world.php?country=";
+        let dhttp = new XMLHttpRequest();
+        let result = document.getElementById("result");
+        dhttp.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE && this.status ==200){
+                result.innerHTML = dhttp.response;
+            }
+        };
+
+        dhttp.open("GET", url+country,true );
+        dhttp.send();
+
+    });
+
+    var citylookup= document.getElementById("cities");
+    citylookup.addEventListener("click", function(){
+        let country = document.getElementById("country").value;
+        let url = "http://localhost/info2180-lab5/world.php?context=";
 
         let dhttp = new XMLHttpRequest();
         let result = document.getElementById("result");
--- world.php
+++ world.php
@@ -10,11 +10,15 @@ $country =  filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
 $context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$cityquery = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$context%'");
+$city = $cityquery->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <?php
     header('Access-Control-Allow-Origin: *');
 ?>
 
+<?php if(isset($country)&&(!isset($context))) :?>
 <table>
           <thead>
             <tr>
@@ -34,4 +38,22 @@ $context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
               </tr>
             <?php endforeach; ?>
           </tbody>
-        </table>
+
+</table>
+
+<?php elseif (isset($context)): ?>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th> 
+    <th>Population</th>
+  </tr>
+  <?php foreach ($city as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+<?php endif ?>
