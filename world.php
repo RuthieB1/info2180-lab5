296f9f3218d883d9cc2ee320a5995df4ee6e69a9
Ruth-Ann Blair <blair.ruthie1@gmail.com>
2022-11-14 22:41:16 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -3,21 +3,35 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 $country =  filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
 ?>
 <?php
     header('Access-Control-Allow-Origin: *');
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table>
+          <thead>
+            <tr>
+              <th>Country Name</th>
+              <th>Continent</th>
+              <th>Independence Year</th>
+              <th>Head of State</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($results as $row): ?>
+              <tr>
+                <td><?= $row['name']?></td>
+                <td><?= $row['continent']?></td>
+                <td><?= $row['independence_year']?></td>
+                <td><?= $row['head_of_state']?></td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
